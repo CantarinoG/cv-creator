afb7cf5c286393227d78fb43092ae41ea1ce0b4c
CantarinoG <guilhermecantarino1709@gmail.com>
2023-01-20 22:27:07 -0300

    Refactor App.js with functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Header";
 import Info from "./components/Info";
@@ -6,142 +6,121 @@ import Preview from "./components/Preview";
 
 import "./styles/App.css";
 
-class App extends Component {
+const App = () => {
 
-  constructor() {
-    super();
+    //Setting states
+    const [personal, setPersonal] = useState({
+      fullName: "",
+      title: "",
+      address: "",
+      phoneNumber: "",
+      email: "",
+      description: "",
+      img: ""
+    });
+    const [experienceList, setExperienceList] = useState([]);
+    const [educationList, setEducationList] = useState([]);
 
-    this.state = {
-      personal: {
-        fullName: "",
-        title: "",
-        address: "",
-        phoneNumber: "",
-        email: "",
-        description: "",
-        img: ""
-      },
-      experienceList: [],
-      //{ position, company, city, from, to }
-      educationList: []
-      //{ university, city, degree, subject, from, to }
+    //Creating the functions
+    const setPersonalInfo = (e) => {
+      const currentPersonal = {
+        fullName: personal.fullName,
+        title: personal.title,
+        address: personal.address,
+        phoneNumber: personal.phoneNumber,
+        email: personal.email,
+        description: personal.description,
+        img: personal.img
+      };
+      const field = e.target.placeholder;
+      if(field === "Full name"){
+        currentPersonal.fullName = e.target.value;
+      } else if (field === "Title"){
+        currentPersonal.title = e.target.value;
+      } else if (field === "Address"){
+        currentPersonal.address = e.target.value;
+      } else if (field === "Phone number"){
+        currentPersonal.phoneNumber = e.target.value;
+      } else if (field === "Email"){
+        currentPersonal.email = e.target.value;
+      } else if (field === "Description"){
+        currentPersonal.description = e.target.value;
+      } else if (e.target.id === "photo"){
+        currentPersonal.photo = URL.createObjectURL(e.target.files[0])
+      }
+      setPersonal(currentPersonal);
     }
-
-    this.setPersonalInfo = this.setPersonalInfo.bind(this);
-
-    this.addNewExperience = this.addNewExperience.bind(this);
-    this.setExperience = this.setExperience.bind(this);
-    this.deleteExperience = this.deleteExperience.bind(this);
-
-    this.addNewEducation = this.addNewEducation.bind(this);
-    this.setEducation = this.setEducation.bind(this);
-    this.deleteEducation = this.deleteEducation.bind(this);
-
-    this.reset = this.reset.bind(this);
-  }
-
-  setPersonalInfo(e){
-    const currentPersonal = this.state.personal;
-    const field = e.target.placeholder;
-    if(field === "Full name"){
-      currentPersonal.fullName = e.target.value;
-    } else if (field === "Title"){
-      currentPersonal.title = e.target.value;
-    } else if (field === "Address"){
-      currentPersonal.address = e.target.value;
-    } else if (field === "Phone number"){
-      currentPersonal.phoneNumber = e.target.value;
-    } else if (field === "Email"){
-      currentPersonal.email = e.target.value;
-    } else if (field === "Description"){
-      currentPersonal.description = e.target.value;
-    } else if (e.target.id === "photo"){
-      currentPersonal.photo = URL.createObjectURL(e.target.files[0])
+  
+    const setExperience = (e) =>{
+      const index = e.target.parentNode.parentNode.id;
+      const currentExperienceList = [...experienceList];
+      const currentExperience = currentExperienceList[index];
+      if(e.target.classList.contains('position')){
+        currentExperience["position"] = e.target.value;
+      } else if(e.target.classList.contains('company')){
+        currentExperience["company"] = e.target.value;
+      } else if(e.target.classList.contains('city')){
+        currentExperience["city"] = e.target.value;
+      } else if(e.target.classList.contains('from')){
+        currentExperience["from"] = e.target.value;
+      } else if(e.target.classList.contains('to')){
+        currentExperience["to"] = e.target.value;
+      }
+      currentExperienceList[index] = currentExperience;
+        setExperienceList(currentExperienceList);
     }
-    this.setState({
-      personal: currentPersonal
-    });
-  }
-
-  setExperience(e){
-    const index = e.target.parentNode.parentNode.id;
-    const currentExperienceList = this.state.experienceList;
-    const currentExperience = currentExperienceList[index];
-    if(e.target.classList.contains('position')){
-      currentExperience["position"] = e.target.value;
-    } else if(e.target.classList.contains('company')){
-      currentExperience["company"] = e.target.value;
-    } else if(e.target.classList.contains('city')){
-      currentExperience["city"] = e.target.value;
-    } else if(e.target.classList.contains('from')){
-      currentExperience["from"] = e.target.value;
-    } else if(e.target.classList.contains('to')){
-      currentExperience["to"] = e.target.value;
+  
+    const addNewExperience = () => {
+      const currentExperienceList = [...experienceList];
+      const emptyExperience = {position: "Position", company: "Company", city: "City", from: "From", to: "To"};
+      currentExperienceList.push(emptyExperience);
+      setExperienceList(currentExperienceList);
     }
-    currentExperienceList[index] = currentExperience;
-      this.setState({experienceList: currentExperienceList});
-  }
-
-  addNewExperience() {
-    const currentExperienceList = this.state.experienceList;
-    const emptyExperience = {position: "Position", company: "Company", city: "City", from: "From", to: "To"};
-    currentExperienceList.push(emptyExperience);
-    this.setState({
-      experienceList: currentExperienceList
-    });
-  }
-
-  deleteExperience(e) {
-    const index = e.target.parentNode.id;
-    const currentExperienceList = this.state.experienceList;
-    currentExperienceList.splice(index, 1);
-    this.setState({
-      experienceList: currentExperienceList
-    });
-  }
-
-  setEducation(e) {
-    const index = e.target.parentNode.parentNode.id;
-    const currentEducationList = this.state.educationList;
-    const currentEducation = currentEducationList[index];
-    if(e.target.classList.contains('university')){
-      currentEducation["university"] = e.target.value;
-    } else if(e.target.classList.contains('edu-city')){
-      currentEducation["city"] = e.target.value;
-    } else if(e.target.classList.contains('degree')){
-      currentEducation["degree"] = e.target.value;
-    } else if(e.target.classList.contains('subject')){
-      currentEducation["subject"] = e.target.value;
-    } else if(e.target.classList.contains('edu-from')){
-      currentEducation["from"] = e.target.value;
-    } else if(e.target.classList.contains('edu-to')){
-      currentEducation["to"] = e.target.value;
+  
+    const deleteExperience = (e) => {
+      const index = e.target.parentNode.id;
+      const currentExperienceList = [...experienceList];
+      currentExperienceList.splice(index, 1);
+      setExperienceList(currentExperienceList);
     }
-    currentEducationList[index] = currentEducation;
-      this.setState({educationList: currentEducationList});
-  }
-
-  addNewEducation() {
-    const currentEducationList = this.state.educationList;
-    const emptyEducation = { university: "University", city: "City", degree: "Degree", subject: "Subject", from: "From", to: "To" };
-    currentEducationList.push(emptyEducation);
-    this.setState({
-      educationList: currentEducationList
-    });
-  }
-
-  deleteEducation(e) {
-    const index = e.target.parentNode.id;
-    const currentEducationList = this.state.educationList;
-    currentEducationList.splice(index, 1);
-    this.setState({
-      educationList: currentEducationList
-    });
-  }
-
-  reset() {
-    this.setState({
-      personal: {
+  
+    const setEducation = (e) => {
+      const index = e.target.parentNode.parentNode.id;
+      const currentEducationList = [...educationList];
+      const currentEducation = currentEducationList[index];
+      if(e.target.classList.contains('university')){
+        currentEducation["university"] = e.target.value;
+      } else if(e.target.classList.contains('edu-city')){
+        currentEducation["city"] = e.target.value;
+      } else if(e.target.classList.contains('degree')){
+        currentEducation["degree"] = e.target.value;
+      } else if(e.target.classList.contains('subject')){
+        currentEducation["subject"] = e.target.value;
+      } else if(e.target.classList.contains('edu-from')){
+        currentEducation["from"] = e.target.value;
+      } else if(e.target.classList.contains('edu-to')){
+        currentEducation["to"] = e.target.value;
+      }
+      currentEducationList[index] = currentEducation;
+        setEducationList(currentEducationList);
+    }
+  
+    const addNewEducation = () => {
+      const currentEducationList = [...educationList];
+      const emptyEducation = { university: "University", city: "City", degree: "Degree", subject: "Subject", from: "From", to: "To" };
+      currentEducationList.push(emptyEducation);
+      setEducationList(currentEducationList);
+    }
+  
+    const deleteEducation = (e) => {
+      const index = e.target.parentNode.id;
+      const currentEducationList = [...educationList];
+      currentEducationList.splice(index, 1);
+      setEducationList(currentEducationList);
+    }
+  
+    const reset = () => {
+      setPersonal({
         fullName: "",
         title: "",
         address: "",
@@ -149,37 +128,36 @@ class App extends Component {
         email: "",
         description: "",
         photo: ""
-      },
-      experienceList: [],
-      educationList: []
-    });
-  }
+      });
+      setExperienceList([]);
+      setEducationList([]);
+    }
 
-  render() {
+    //Render
     return (
       <div className="app">
         <Header/>
         <Info
-        personal={this.state.personal}
-        setPersonalInfo={this.setPersonalInfo}
-        experienceList={this.state.experienceList}
-        addNewExperience={this.addNewExperience}
-        setExperience={this.setExperience}
-        deleteExperience={this.deleteExperience}
-        educationList={this.state.educationList}
-        addNewEducation={this.addNewEducation}
-        setEducation={this.setEducation}
-        deleteEducation={this.deleteEducation}
-        reset={this.reset}
+        personal={personal}
+        setPersonalInfo={setPersonalInfo}
+        experienceList={experienceList}
+        addNewExperience={addNewExperience}
+        setExperience={setExperience}
+        deleteExperience={deleteExperience}
+        educationList={educationList}
+        addNewEducation={addNewEducation}
+        setEducation={setEducation}
+        deleteEducation={deleteEducation}
+        reset={reset}
         />
         <Preview
-        personal={this.state.personal}
-        experienceList={this.state.experienceList}
-        educationList={this.state.educationList}
+        personal={personal}
+        experienceList={experienceList}
+        educationList={educationList}
         />
       </div>
     )
-  }
+
 }
 
 export default App;
